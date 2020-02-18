6076a3a1d7deb5496800919e87408455c305b88f
Anikynetix <A.N.I.K@live.com>
2020-02-18 16:37:38 -0600

    Added parsers to populate stateMapper

--- driver.py
+++ driver.py
@@ -6,19 +6,79 @@ Created on Feb 16, 2020
 
 import re
 import time
+
 from z3 import *
 
 pathToGraphDir = "digraphs/"
+stateMapper = {}
 
 
-def getStates():
+def populateMapper(lines):
+
+    for line in lines:
+
+        stateFrom, stateTo, isLoop = getStates(line)
+        stateLabel = getLabel(line)
+        
+        if (stateFrom != None or  stateTo != None or stateLabel != None):
+
+            # stateMapper[stateFrom] = []
+
+            stateMapper[stateFrom].append([stateTo, stateLabel])
+
+
+def createStatesInMapper(lines):
+
+    for line in lines:
+
+        try:
+
+            match = re.compile("\"\(.*\)\" \[label=\"\(")
+            label = match.findall(line)[0][2:-12]
+
+            stateMapper[label] = []
+
+        except:
+
+            # print("Given line :", line, "could not be parsed.")
+            pass
+
+
+def getStates(line):
     
-    return
+    # print(line)
+
+    try:
+
+        match = re.compile("\(.*\)\" -> \"\(")
+        stateFrom = match.findall(line)[0][1:-8]
+
+        match = re.compile("\)\" -> \"\(.*\)\" \[label = \"\(")
+        stateTo = match.findall(line)[0][8:-14]
+
+        return stateTo, stateFrom, stateTo == stateFrom
+
+    except:
+
+        # print("Given line :", line, "could not be parsed.")
+        return None, None, False
 
 
-def getLabel():
-    
-    return
+def getLabel(line):
+
+    # print(line)
+
+    try:
+        
+        match = re.compile("\)\" \[label = \"\(.*\)\"\]")
+        label = match.findall(line)[0][14:-3]
+
+        return label
+
+    except:
+
+        # print("Given line :", line, "could not be parsed.")
+        return None
 
 
 def getLTL3(fileName):
@@ -29,7 +89,6 @@ def getLTL3(fileName):
     file = open(path)
     
     line = file.readline()
-    ltl3.append(line.rstrip())
     
     while line:
         
@@ -44,7 +103,24 @@ def getLTL3(fileName):
 def main():
     
     testFile = "a U b.txt"
-    print(getLTL3(testFile))
+
+    ltl3 = getLTL3(testFile)
+    # print(populateMapper(ltl3[0]))
+
+    createStatesInMapper(ltl3)
+    populateMapper(ltl3)
+
+    # print(createStatesInMapper(ltl3))
+
+#     mapper = {}
+#     s = []
+#     s.append("aj")
+#
+#     mapper["anik"] = []
+#     mapper["anik"].append("ax")
+#     mapper["anik"].append("ab")
+
+    print(stateMapper)
 
 
 if __name__ == '__main__':
